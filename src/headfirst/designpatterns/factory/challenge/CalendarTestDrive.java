512f37a64c081c89c5c38eb49c1a1e5aa66c707d
Eng.  Abhinandan H. Patil <abhinandan_patil_1414@yahoo.com>
2023-08-10 03:58:05 +0530

    Latest

--- src/headfirst/designpatterns/factory/challenge/CalendarTestDrive.java
+++ src/headfirst/designpatterns/factory/challenge/CalendarTestDrive.java
@@ -7,7 +7,7 @@ public class CalendarTestDrive {
 	public static void main(String[] args) {
 		ZoneFactory zoneFactory = new ZoneFactory();
 		Calendar calendar = new PacificCalendar(zoneFactory);
-		List<String> appts = Arrays.asList("appt 1", "appt 2");
+		List<String> appts = Arrays.asList("appt 1", "appt 2","appt 3");
 		calendar.createCalendar(appts);
 		calendar.print();
 	}
--- src/headfirst/designpatterns/factory/challenge/PacificCalendar.java
+++ src/headfirst/designpatterns/factory/challenge/PacificCalendar.java
@@ -3,13 +3,18 @@ package headfirst.designpatterns.factory.challenge;
 import java.util.*;
 
 public class PacificCalendar extends Calendar {
+    List<String> calenderAppointments = new ArrayList<String>();
+		
 	public PacificCalendar(ZoneFactory zoneFactory) {
 		zone = zoneFactory.createZone("US/Pacific");
 		// make a calendar for the pacific zone
 		// ...
 	}
 	public void createCalendar(List<String> appointments) {
+		calenderAppointments.addAll(appointments);
+
 		// make calendar from appointments
 		System.out.println("Making the calendar");
+		System.out.println("Printing all the appointments"+calenderAppointments.toString());
 	}
 }
