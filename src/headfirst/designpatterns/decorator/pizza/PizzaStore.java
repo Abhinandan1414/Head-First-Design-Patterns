d9e18c5096f982e12d08891f60c032df8d4dda73
Eng.  Abhinandan H. Patil <abhinandan_patil_1414@yahoo.com>
2023-08-10 04:20:56 +0530

    Update PizzaStore.java

--- src/headfirst/designpatterns/decorator/pizza/PizzaStore.java
+++ src/headfirst/designpatterns/decorator/pizza/PizzaStore.java
@@ -7,6 +7,10 @@ public class PizzaStore {
 		Pizza cheesePizza = new Cheese(pizza);
 		Pizza greekPizza = new Olives(cheesePizza);
 
+		System.out.println(pizza.getDescription() 
+				+ " $" + pizza.cost());
+		System.out.println(cheesePizza.getDescription() 
+				+ " $" + cheesePizza.cost());
 		System.out.println(greekPizza.getDescription() 
 				+ " $" + greekPizza.cost());
 
